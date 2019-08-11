0d351d9840c2e7bb721d3bbb3805faedb7b96c0c
André Pedruzzi <andrerpedruzzi@gmail.com>
2019-08-10 22:47:12 -0300

    realtime added

--- src/server.js
+++ src/server.js
@@ -4,15 +4,30 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    console.log(user, socket.id);
+    connectedUsers[user] = socket.id;
+})
 
 mongoose.connect(process.env.DB_URL, { 
     useNewUrlParser: true
 });
 
-server.use(cors());
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers; 
+    return next();
+})
 
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333, console.log('Running on port 3333'));
